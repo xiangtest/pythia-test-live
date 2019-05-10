60c48bf3039b5c49b929373a8b79c286e3d9713c
Stallion <xiang5337@hotmail.com>
2019-05-10 15:18:44 +0800

    1061141 Bug test case 5.2.3 (#6)
    1061141 
    
    - Task test case 5.2.3
    
    Ticket: 1061141
    Reviewer: Stallion

--- lumen-framework/bootstrap/app.php
+++ lumen-framework/bootstrap/app.php
@@ -11,7 +11,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 | Create The Application
 |------------------------- - - --- -- -- -- -- -- --
 |
-| Here we will load the environment and create the application instance
+|test Here we will load the environment and create the application instance
 | that serves as the central piece of this framework. We'll use this
 | application as an "IoC" container and router for this framework.
 |
