60369e137b846ac5b355db4160e3ed5f4a6bb87d
Graig Patrick <graig.patrick@gmail.com>
2019-05-15 13:08:46 +0800

    fixed bug

--- lumen-framework/bugcaches/IgnoreFileAnBugCacheCase1.php
+++ lumen-framework/bugcaches/IgnoreFileAnBugCacheCase1.php
@@ -1,15 +1,21 @@
 <?php
-class mock {
 
-        public function method1UpdatedByPrerequisite2()
-        {
-                //code goes here
-        }
+class mock
+{
 
-        public function method2()
-        {
-                $variable = "string tests";
-                return $variable;
-        }
+    public function method1UpdatedByPrerequisite2()
+    {
+        //code goes here
+        $variable = "string tests";
+
+        return $variable;
+    }
+
+    public function method2()
+    {
+        $variable = "string tests";
+
+        return $variable;
+    }
 }
 
